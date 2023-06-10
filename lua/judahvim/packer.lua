b7ea76dbeae2e76cdafb241cffe7baedaba0fe67
Judah Sullivan <judahsullivan.dev@gmail.com>
2023-06-10 03:48:30 -0500

    made much needed config updates tp the packer,remap,settings

--- lua/judahvim/packer.lua
+++ lua/judahvim/packer.lua
@@ -21,6 +21,7 @@ return require('packer').startup(function(use)
     require("nvim-tree").setup {}
   end
 }
+  use 'norcalli/nvim-colorizer.lua'
   use({
 	  'rose-pine/neovim',
 	  as = 'rose-pine',
@@ -47,12 +48,15 @@ return require('packer').startup(function(use)
 				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
 				ts_update()
 			end,}
-
-  use("theprimeagen/refactoring.nvim")
-  use("mbbill/undotree")
-  use("tpope/vim-fugitive")
+  -- use 'github/copilot.vim'
+  use 'lewis6991/gitsigns.nvim'
+  use 'dinhhuy258/git.nvim'
   use("nvim-treesitter/nvim-treesitter-context");
-
+  use 'nvim-lualine/lualine.nvim' -- Statusline
+  use 'onsails/lspkind-nvim' -- vscode-like pictograms
+  use 'windwp/nvim-autopairs'
+  use 'windwp/nvim-ts-autotag'
+  use 'akinsho/nvim-bufferline.lua'
   use {
 	  'VonHeikemen/lsp-zero.nvim',
 	  branch = 'v1.x',
